7c49babb66f29687e34ceb160af807fba4e0e5d6
zyxialing <1039574335@qq.com>
2021-12-23 23:17:00 +0800

    ui摄像机

--- Runtime/Tools/CameraUtils.cs
+++ Runtime/Tools/CameraUtils.cs
@@ -7,9 +7,9 @@ public class CameraUtils
 
     public static void SetUICameraParma(Camera camera)
     {
-        camera.clearFlags = CameraClearFlags.SolidColor;
-        camera.backgroundColor = Color.black;
+        camera.clearFlags = CameraClearFlags.Nothing;
         camera.cullingMask = 1 << 5;
+        camera.depth = 9000;
         camera.orthographic = true;
         camera.transform.position = Vector3.back * 10000;
     }
